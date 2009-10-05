31617a208bd3b434acc07a86fa4a65500a0ef0d1
Steve <sivy@fairbairn-sykes.local>
2009-10-04 20:27:33 -0700

    add

---
+++ urls.py
@@ -0,0 +1,26 @@
+from os.path import join, dirname
+
+from django.conf.urls.defaults import *
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^idact/', include('idact.foo.urls')),
+
+    url(r'^server/', include('identity.urls')),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': join(dirname(__file__), 'static')},
+        name='static'),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    #(r'^admin/', include(admin.site.urls)),
+)
